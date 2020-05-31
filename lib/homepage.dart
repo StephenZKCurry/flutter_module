6d6a72dc5fbb84ada4718eb4d22fa291ce29c1b1
StephenZKCurry <2029386632@qq.com>
2020-05-31 16:00:29 +0800

    修改页面样式

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -62,37 +62,50 @@ class _HomePageState extends State<HomePage> {
         automaticallyImplyLeading: false,
       ),
       body: Container(
+        padding: EdgeInsets.all(15.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Flutter页面，route=${widget.route}，原生页面传过来的参数：name=${widget.params['name']}',
+              '我是Flutter页面，route=${widget.route}，原生页面传过来的参数：name=${widget.params['name']}',
               style: TextStyle(fontSize: 16.0),
             ),
             TextField(
               controller: _nameController,
+              decoration: InputDecoration(hintText: "请输入要传递到原生端的参数"),
             ),
-            RaisedButton(
-                child: Text('返回上一页'),
-                onPressed: () {
-                  Map<String, dynamic> result = {'message': '我从Flutter页面回来了'};
-                  nativeChannel.invokeMethod('goBackWithResult', result);
-                }),
-            RaisedButton(
-                child: Text('跳转Flutter页面'),
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return SecondPage();
-                  }));
-                }),
-            RaisedButton(
-                child: Text('跳转Android原生页面'),
-                onPressed: () {
-                  // 跳转原生页面
-                  Map<String, dynamic> result = {'name': _nameController.text};
-                  nativeChannel.invokeMethod('jumpToNative', result);
-                }),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                  child: Text('返回上一页'),
+                  onPressed: () {
+                    Map<String, dynamic> result = {'message': '我从Flutter页面回来了'};
+                    nativeChannel.invokeMethod('goBackWithResult', result);
+                  }),
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                  child: Text('跳转Flutter页面'),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return SecondPage();
+                    }));
+                  }),
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                  child: Text('跳转Android原生页面'),
+                  onPressed: () {
+                    // 跳转原生页面
+                    Map<String, dynamic> result = {
+                      'name': _nameController.text
+                    };
+                    nativeChannel.invokeMethod('jumpToNative', result);
+                  }),
+            ),
           ],
         ),
       ),
